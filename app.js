66699c5ead97d74ac5f4f8d53a2fa2579b5b8228
danielp14 <danielpintostec@gmail.com>
2022-12-02 10:30:30 -0300

    last

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "-----",
+  apiKey: "",
   authDomain: "crud-incone-22.firebaseapp.com",
   projectId: "crud-incone-22",
   storageBucket: "crud-incone-22.appspot.com",
@@ -68,22 +68,118 @@ function login(email, password){
     // ...
   })
   .catch((error) => {
+    console.log(error);
     const errorCode = error.code;
+    console.log(errorCode);
     const errorMessage = error.message;
-    console.log(errorMessage);
+    //aca deberia pasar algo
+    showError(errorMessage);
+
   });
 }
 
+
+
+function showError(err) {
+  let inputsError = document.querySelectorAll('.form-control-auth');
+  //forma declarativa
+  inputsError.forEach(element => {
+    element.classList.add('error-input');
+  });
+
+  document.querySelector('#error-auth').innerHTML = getErrorMsg(err);
+  //vamos colorear en rojo los input
+}
+
+function getErrorMsg(code) {
+  console.log(code);
+  let msg;
+
+  switch (code) {
+    case 400:
+      msg = 'Usuario no registrado';
+      break;
+    case 401:
+      msg = 'Credenciales invlidas';
+      break;
+    default:
+      msg = 'Fallo en Autenticación';
+  }
+
+  return msg;
+}
+
+function validateField(field = '', type){
+  let status = false,
+      msg = '';
+  const rules = /^[A-Za-z0-9_.-]*$/;
+  const rulesMail = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+  let input = field.trim();
+  switch (type) {
+    case 'password':
+      if ( input.length > 5 && rules.test(input) ){
+        status = true;
+      } else {
+        msg = 'La contraseña debe tener al menos 5 caracteres. Solo se permiten letras, números, -_ y .'
+      }
+      break;
+    case 'email':
+      if(rulesMail.test(input)) {
+        status = true;
+      } else {
+        msg = 'La dirección de correo electrónico no es válida.';
+      }
+      break;
+    default:
+      //nunca este vacio
+      if(input.length > 3) {
+        status = true;
+      } else {
+        msg = 'Ingrese más de 3 caracteres';
+      }
+      break;
+  }
+
+  return {status, msg};
+}
 const modalAuth = document.getElementById('modalAuth');
+const btnAction = modalAuth.querySelector('#btn-action');
+let action = null;
 modalAuth.addEventListener('show.bs.modal', (event) =>{
   const button = event.relatedTarget;
-  const action = button.getAttribute('data-action-form');
-  console.log('>>>'+action);
+  action = button.getAttribute('data-action-form');
 
   const modalTitle = modalAuth.querySelector('.modal-title');
   modalTitle.textContent = action === 'login' ? 'Ingresar' : 'Registrarse';
 
-  const btnAction = modalAuth.querySelector('#btn-action');
   btnAction.innerHTML = action === 'login' ? 'Ingresar' : 'Registrarse';
-
 });
+
+//SCOPE
+btnAction.addEventListener('click', (event) => {
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
+  
+  let validateMail = validateField(email, 'email');
+  let validatePassword = validateField(password, 'password');
+  if( validateMail.status && validatePassword.status ){
+    //esto paso la validacion
+    sendRequest(email, password);
+  } else {
+    console.log('fallo validacion '+ validateMail.msg + validatePassword.msg);
+
+  }
+
+})
+
+function sendRequest(email, password) {
+  if (action === 'login'){
+    //llamamos la funcion login
+    login( email, password);
+  } else {
+    //llamamos al register
+    registerUser(email, password);
+  }
+}
+
+
